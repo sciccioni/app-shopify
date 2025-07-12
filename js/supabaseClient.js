944052e664eceba2aa45a9417900d2f32458f016
sciccioni <sciccioni@photosi.com>
2025-07-12 17:44:38 +0200

    ripulitura

--- js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/dist/esm/index.js';
-
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
