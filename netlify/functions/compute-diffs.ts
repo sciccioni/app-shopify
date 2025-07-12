e2947c8a68a0ce97feb0e61550721ff9b699cf07
sciccioni <sciccioni@photosi.com>
2025-07-12 17:55:04 +0200

    Update compute-diffs.ts

--- netlify/functions/compute-diffs.ts
+++ netlify/functions/compute-diffs.ts
@@ -1,1 +1,10 @@
+// netlify/functions/compute-diffs.ts
+import { Handler } from '@netlify/functions';
+import { DiffService } from '../../src/services/DiffService';
+
+export const handler: Handler = async (evt) => {
+  const { import_id } = JSON.parse(evt.body!);
+  const diffs = await DiffService.run(import_id);
+  return { statusCode: 200, body: JSON.stringify(diffs) };
+};
 
